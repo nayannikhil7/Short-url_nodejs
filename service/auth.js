44603109b0171b054aacee86af2c30af21d9cecf
NIKHIL <nn21mmb0a46@student.nitw.ac.in>
2024-06-05 15:18:37 +0530

    implemented JWT

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,35 @@
-const sessionIdToUserMap = new Map();
+// const sessionIdToUserMap = new Map();
+//we will now use jwt
 
-function setUser(id, user) {
-    sessionIdToUserMap.set(id, user);
+const jwt = require("jsonwebtoken")
+const secret = "Nayan$123@"
+
+// function setUser(id, user) {
+//     sessionIdToUserMap.set(id, user);
+// }
+
+//id ki jrurat nhi h as we are creating short ids
+function setUser(user) {
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    },
+        secret
+    )
 }
 
-function getUser(id) {
-    return sessionIdToUserMap.get(id);
+// function getUser(id) {
+//     return sessionIdToUserMap.get(id);
+// }
+
+function getUser(token) {
+    if (!token) return null;
+    try {
+        return jwt.verify(token, secret);
+
+    } catch (error) {
+        return null;
+    }
 }
 
 module.exports = {
